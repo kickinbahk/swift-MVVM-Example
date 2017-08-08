f33cf02a0cdc8f9c9bead06f01d991f2a30cb3be
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-08-08 07:15:15 -0700

    add generic to handle view changes

---
+++ MVVMSwiftExample/Dynamic.swift
@@ -0,0 +1,33 @@
+//
+//  Dynamic.swift
+//  MVVMSwiftExample
+//
+//  Created by Josiah Mory on 8/8/17.
+//  Copyright © 2017 Toptal. All rights reserved.
+//
+
+import Foundation
+
+class Dynamic<T> {
+  typealias Listener = (T) -> ()
+  var listener: Listener?
+  
+  func bind(_ listener: Listener?) {
+    self.listener = listener
+  }
+  
+  func bindAndFire(_ listener: Listener) {
+    self.listener = listener
+    listener?(value)
+  }
+  
+  var value: T {
+    didSet {
+      listener?(value)
+    }
+  }
+  
+  init(_ v: T) {
+    value = v
+  }
+}
